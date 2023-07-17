a20f148867e897e81f4b1d1ab48c124c6a7f352a
Saraht <Goldilocks.13.a@gmail.com>
2023-07-18 00:25:27 +0400

    Initial commit

--- fill_infoblock.php
+++ fill_infoblock.php
@@ -18,7 +18,41 @@ if ($iblockID) {
             'COORDINATES' => '55.753930, 37.620795',
             'CITY' => 'Москва',
         ),
-        // Добавьте остальные объекты
+        array(
+            'NAME' => 'Объект 2',
+            'PHONE' => '+987654321',
+            'EMAIL' => 'object2@example.com',
+            'COORDINATES' => '55.123456, 37.987654',
+            'CITY' => 'Санкт-Петербург',
+        ),
+        array(
+            'NAME' => 'Объект 3',
+            'PHONE' => '+789456123',
+            'EMAIL' => 'object3@example.com',
+            'COORDINATES' => '56.854612, 35.825469',
+            'CITY' => 'Тверь',
+        ),
+        array(
+            'NAME' => 'Объект 4',
+            'PHONE' => '+456789123',
+            'EMAIL' => 'object4@example.com',
+            'COORDINATES' => '53.227283, 50.237634',
+            'CITY' => 'Самара',
+        ),
+        array(
+            'NAME' => 'Объект 5',
+            'PHONE' => '+321654987',
+            'EMAIL' => 'object5@example.com',
+            'COORDINATES' => '56.838011, 60.597465',
+            'CITY' => 'Екатеринбург',
+        ),
+        array(
+            'NAME' => 'Объект 6',
+            'PHONE' => '+654789321',
+            'EMAIL' => 'object6@example.com',
+            'COORDINATES' => '59.934280, 30.335099',
+            'CITY' => 'Санкт-Петербург',
+        ),
     );
 
     $element = new CIBlockElement;
